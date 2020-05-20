b662db2c000b40d9451698a20d51dcc92789e846
Malte Wirz <mail@maltewirz.com>
2020-05-20 08:50:41 +0200

    email password authentication working

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,6 +2,28 @@
     <aside>
         <h3>Sign in Anonymously</h3>
         <button @click="auth.signInAnonymously()">Sign in</button>
+
+        <div v-if="newUser">
+            <h3>Sign Up for a new Account</h3>
+            <a href="#" @click="newUser = false">Returning User?</a>
+        </div>
+
+        <div v-else>
+            <h3>Sign In with Email</h3>
+            <a href="#" @click="newUser = true">New user?</a>
+        </div>
+
+        <input v-model="email" placeholder="email" type="email" class="input">#
+        <input v-model="password" type="password" class="input">
+
+        <br>
+
+        <button class="button is-info" :class="{ 'is-loading': loading}" @click="signInOrCreateUser()">
+            {{ newUser ? 'Sign Up' : 'Login' }}
+        </button>
+
+        <p class="has-text-danger" v-if="errorMessage"> {{ errorMessage }}</p>
+
     </aside>
 </template>
 
@@ -12,6 +34,29 @@ export default {
     data() {
         return {
             auth,
+            newUser: false,
+            email: '',
+            password: '',
+            loading: false,
+            errorMessage: ''
+        }
+    },
+
+    methods: {
+        async signInOrCreateUser() {
+            this.loading = true;
+            this.errorMessage = '';
+            try {
+                if (this.newUser) {
+                    await auth.createUserWithEmailAndPassword(this.email, this.password);
+                } else {
+                    await auth.signInWithEmailAndPassword(this.email, this.password);
+                }
+            } catch (error) {
+                this.errorMessage = error;
+            }
+
+            this.loading = false;
         }
     }
     
