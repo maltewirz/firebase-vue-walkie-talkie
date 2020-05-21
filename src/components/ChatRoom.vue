79317b3bd4837946804ba96c8112f2ba09d212f5
Malte Wirz <mail@maltewirz.com>
2020-05-21 12:21:30 +0200

    dynamic routing

---
+++ src/components/ChatRoom.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>Welcome to ChatRoom {{ chatId }}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            chatId() {
+                return this.$route.params.id;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
