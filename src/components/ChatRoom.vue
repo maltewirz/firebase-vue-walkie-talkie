42af21ca87394b8afc5b286263686d0cac907639
Malte Wirz <mail@maltewirz.com>
2020-05-21 12:45:48 +0200

    presentation of ui

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -5,7 +5,10 @@
         <User #user="{ user }">
             <ul>
                 <li v-for="message of messages" :key="message.id">
-                    {{ message.text }}
+                    <ChatMessage 
+                        :message="message"
+                        :owner="user.uid === message.sender"
+                    />
                 </li>
             </ul>
 
@@ -25,11 +28,13 @@
 
 <script>
 import User from './User';
+import ChatMessage from './ChatMessage';
 import { db } from '../firebase';
 
 export default {
     components: {
         User,
+        ChatMessage
     },
     data() {
         return {
@@ -73,5 +78,20 @@ export default {
 </script>
 
 <style scoped>
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  min-width: 500px;
+  background: #efefef;
+  padding: 10px;
+  border-radius: 0;
+}
+li {
+  display: flex;
+}
 
 </style>
