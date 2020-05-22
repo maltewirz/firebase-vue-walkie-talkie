6bc8bdff6d6ae5d1f5a51040e0aa42328a88a6ff
Malte Wirz <mail@maltewirz.com>
2020-05-22 16:12:57 +0200

    audio recording

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -13,9 +13,18 @@
             </ul>
 
             <input v-model="newMessageText" class="input" />
+            <hr>
+            <h5>Record Audio</h5>
 
+            <button v-if="!recorder" @click="record()">Record</button>
+            <button v-else @click="stop()">Stop</button>
+
+            <audio v-if="newAudio" :src="newAudioURL" controls></audio>
+
+
+            <hr>
             <button
-                :disabled="!newMessageText || loading"
+                :disabled="(!newMessageText && !newAudio) || loading"
                 class="button is-success"
                 type="text"
                 @click="addMessage(user.uid)">
@@ -40,7 +49,9 @@ export default {
         return {
             newMessageText: '',
             loading: false,
-            messages: []
+            messages: [],
+            newAudio: null,
+            recorder: null,
         }
     },
     computed: {
@@ -49,6 +60,9 @@ export default {
         },
         messagesCollection() {
             return db.doc(`chats/${this.chatId}`).collection('messages');
+        },
+        newAudioURL() {
+            return URL.createObjectURL(this.newAudio);
         }
     },
     // vuefire
@@ -71,7 +85,36 @@ export default {
 
             this.loading = false;
             this.newMessageText = '';
+        },
+        async record() {
+            this.newAudio = null;
 
+            const stream = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+                video: false,
+            });
+
+            const options = { mimeType: "audio/webm" };
+            const recordedChunks = [];
+
+            this.recorder = new MediaRecorder(stream, options);
+
+            this.recorder.addEventListener("dataavailable", e => {
+                if (e.data.size > 0) {
+                    recordedChunks.push(e.data);
+                }
+            });
+
+            this.recorder.addEventListener("stop", () => {
+                this.newAudio = new Blob(recordedChunks);
+                console.log(this.newAudio)
+            });
+            this.recorder.start();
+
+        },
+        async stop() {
+            this.recorder.stop();
+            this.recorder = null;
         }
     }
 }
